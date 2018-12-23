a29fd68d221dfd29409aac15202a8852cde8be7c
Makara Sok <maso@splitmedialabs.com>
2018-12-23 20:33:47 +0800

    Disable upgrade for socket.io

--- frontend/src/Websocket.js
+++ frontend/src/Websocket.js
@@ -1,7 +1,11 @@
 // with ES6 import
 import io from 'socket.io-client';
 
-const socket = io('/');
+const socket = io('/', {
+  upgrade: false,
+  transports: ['websocket'],
+});
+
 socket.on('connect', function() {
   console.log('🚀 Websocket connected');
 });
